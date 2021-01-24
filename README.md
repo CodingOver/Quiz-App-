4b5eba13bf2f929483ff90e6931b86ee0ad5f59c
Ayoub Fellat <paypal1business.over@gmail.com>
2021-01-24 10:23:58 -0800

    create main.js && Fetch Number Of Question From JSON && Show The Number Of Bullets Depend On Quesions Count && Set The Number For The Right & Wrong Answer && Check Answer and Hide The Question Then Show Next One && If Question Finished Show The Result

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Quiz-App-
+
+Done
+01 - Create JSON Object For Question
+02 - Start Creating HTML + Styling
+03 - Add Dummy Data
+04 - Create The Styling 
+05 - Fetch Number Of Question From JSON
+06 - Show Question Count + Question Category
+07 - Show The Number Of Bullets Depend On Quesions Count
+08 - Set The Number For The Right & Wrong Answer
+10 - Fetch The Question From The JSON Object
+11 - Show First Question
+12 - Check Answer and Hide The Question Then Show Next One
+14 - Add Class on Bullet After Answering The Question 
+15 - If Question Finished Show The Result
+
+In Pregress
+
+09 - Create Countdown Timer
+13 - If The Time End, Mark The Question As Wrong and Show Next Question
+
+
+Ideas 
+- Many Json Object and Choose Language
+- Add Many Question and Choose Only 10 Randomly 
+- Randomize Answers ( Shuffle Function )
---
+++ main.js
@@ -0,0 +1,156 @@
+// Select Element
+let countSpan = document.querySelector('.count span');
+let bullets = document.querySelector('.bullets');
+let bulletsSpanContainer = document.querySelector('.bullets .spans');
+let quizzArea = document.querySelector('.quiz-area');
+let answersArea = document.querySelector('.answers-area');
+let submitButton = document.getElementById('submit-button');
+let answer = document.querySelector('.answer');
+let resultsContainer = document.querySelector('.results');
+// Set Options
+let currentIndex = 0;
+let rightAnswers = 0;
+function getQuestions() {
+  let myRequest = new XMLHttpRequest();
+    
+  myRequest.onload = function() {
+      if(this.status === 200) {
+        let questionObject = JSON.parse(this.responseText);
+        let qCount = questionObject.length;
+
+        // Create Bullets + Set Question Count
+        createBullets(qCount);
+        // Add Qestion Data
+        addQuestionData(questionObject[currentIndex],qCount);
+
+         // Click On Submit
+        submitButton.onclick = () => {
+            // Get Right Answer 
+            let theRightAnswer = questionObject[currentIndex].correct_answer;
+
+            // Incrase Index
+            currentIndex++;
+
+            // check The Answer 
+            checkAnswer(theRightAnswer, qCount);
+
+            // Remove Preivous Question 
+            quizzArea.innerHTML = '';
+            answersArea.innerHTML = '' ;
+
+            // Add Qestion Data
+            addQuestionData(questionObject[currentIndex],qCount);
+
+            // Handle Bullets class
+            handleBullets();
+            
+            // Show Results
+            showResults(qCount);
+        };
+
+      }
+    };
+
+  myRequest.open("GET", "html_question.json", true);
+  myRequest.send();
+}
+
+getQuestions();
+
+function createBullets(num) {
+    countSpan.innerHTML = num;
+
+    // Create Spans
+    for (let i = 0; i < num; i++) {
+        // Create Span
+        bulletsSpanContainer.innerHTML += `<span></span>`;
+        // check if its first span
+        if (i === 0 ) {
+            bulletsSpanContainer.innerHTML = `<span class="on"></span>`;
+        }
+    }
+}
+
+function addQuestionData(obj, count) {
+    if(currentIndex < count) {
+        // Create H2 Question Title
+        quizzArea.innerHTML = `<h2>${obj.question}</h2>`;
+
+        // Create  Answers
+        answersArea.innerHTML = `
+            <div class="answer">
+                <input data-answer="${obj.answer_1}" type="radio" id="answer_1" name="questions" checked>
+                <label for="answer_1" >${obj.answer_1}</label>
+            </div>
+            <div class="answer">
+                <input data-answer="${obj.answer_2}" type="radio" id="answer_2" name="questions">
+                <label for="answer_2">${obj.answer_2}</label>
+            </div>
+            <div class="answer">
+                <input data-answer="${obj.answer_3}" type="radio" id="answer_3" name="questions">
+                <label for="answer_3">${obj.answer_3}</label>
+            </div>
+            <div class="answer">
+                <input data-answer="${obj.answer_4}" type="radio" id="answer_4" name="questions">
+                <label for="answer_4">${obj.answer_4}</label>
+            </div>
+    `;
+    }
+}
+
+function checkAnswer(rAnswer, count) {
+    let answers = document.getElementsByName('questions');
+    let theChoosenAnswer;
+
+    for (let i = 0; i < answers.length; i++)  {
+
+        if (answers[i].checked) {
+            theChoosenAnswer = answers[i].dataset.answer;
+        }
+    }
+
+    console.log(`Right Answer is: ${rAnswer}`);
+    console.log(`choosen Answer is: ${theChoosenAnswer}`);
+
+    if (rAnswer === theChoosenAnswer) {
+        rightAnswers++;
+        console.log('good');
+    }
+}
+
+function handleBullets() {
+    let bulletsSpans = document.querySelectorAll('.bullets .spans span');
+    let arrayOfSpans = Array.from(bulletsSpans);
+
+    arrayOfSpans.forEach((span,index) => {
+        
+        if (currentIndex === index) {
+            span.className = 'on';
+        }
+
+    });
+}
+
+function showResults(count) {
+    let theResults; 
+    if (currentIndex === count) {
+        quizzArea.remove();
+        answersArea.remove();
+        submitButton.remove();
+        bullets.remove();
+
+        if (rightAnswers > (count/2) && rightAnswers < count) {
+            theResults = `<span class="good">Good</span>, ${rightAnswers} From ${count} is Good`;
+        } else if (rightAnswers === count) {
+            theResults = `<span class="perfect">Congratulation</span>, All Answers Is Correct`;
+        } else {
+            theResults = `<span class="bad">Failled!!</span>, ${rightAnswers} From ${count} is Bad`;
+        }
+
+
+        resultsContainer.innerHTML = theResults ;
+        resultsContainer.style.padding = '10px';
+        resultsContainer.style.backgroundColor = 'white';
+        resultsContainer.style.margin = '10px'
+    }
+}
